3df2e91528bb0ec6c14e4b00195c5d5fc9b3ac62
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-17 19:44:40 -0400

    added to play round to append div to instructions after round

--- RPS.js
+++ RPS.js
@@ -3,6 +3,7 @@ let compScore = 0, playerScore = 0;
 const options = ['rock', 'paper', 'scissors'];
 const results = document.querySelector('.results');
 const score = document.querySelector('.score');
+const instructions = document.querySelector('.instructions');
 
     //plays 5 rounds and declares a winner
 //playGame();
@@ -45,9 +46,11 @@ function getPlayerSelection(playerInput){
 
 //compare two values to see who wins
 function playRound(playerChoice, compChoice){
+    let div = document.createElement('div');
+    
     //check if the two are equal 
     if (compChoice === playerChoice){
-        return 'there was tie'
+        div.textContent = 'there was tie';
         //console.log("there was a tie");
     }
 
@@ -56,22 +59,24 @@ function playRound(playerChoice, compChoice){
         if (compChoice===options[i]){ //stop at whatever option comp made to check agains player option
             if (i == 0 && playerChoice===options[2]){ //first check for exception to rule of one behind comp choice in options
                 compScore++;
-                return 'Computer wins'
+                div.textContent = 'Computer wins';
             }else if (playerChoice===options[i-1]){ //if the player choice is one behind in array then computer wins 
                 compScore++;
-                return 'Computer wins';
+                div.textContent = 'Computer wins';
             } else {
                 playerScore++;
-                return 'Player wins';//else player wins
+                div.textContent = 'Player wins';//else player wins
             }
         }
     }
+
+    return div;
 }
 
-function playGame(playerChoice, results){
+function playGame(playerChoice){
    // while (playerScore < 5 && compScore < 5){
         if ( playerScore == 5 || compScore == 5){
-            console.log("Game over");
+            //console.log("Game over");
             gameOver(results);
             return;
         }
@@ -84,7 +89,8 @@ function playGame(playerChoice, results){
         if (!playerChoice){
             console.log('Invalid input! Restart game!')
         }else{
-            console.log("You chose " + playerChoice + " and the computer chose " + compChoice + ", the result: " + playRound(playerChoice,compChoice));
+            //console.log("You chose " + playerChoice + " and the computer chose " + compChoice + ", the result: " + playRound(playerChoice,compChoice));
+            instructions.appendChild(playRound(playerChoice, compChoice));
             dispScore(score);
             //console.log('The current score is player: ' + playerScore + " computer: " + compScore)
         }
@@ -106,5 +112,5 @@ let buttons = buttonOptions.querySelectorAll('.option');
 
 
 buttons.forEach(button => button.addEventListener('click',() => {
-    playGame(button.textContent, results);
+    playGame(button.textContent);
 }));
