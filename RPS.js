03a1cc988b938c0ce98b10856d039e6939a60669
Andrew Linares <andrewlinaresch@gmail.com>
2023-09-18 20:29:22 -0400

    Sentence format of round results changed

--- RPS.js
+++ RPS.js
@@ -39,7 +39,7 @@ function getPlayerSelection(playerInput){
 //Returns an h3 element with round winner
 function playRound(playerChoice, compChoice){
     let h3 = document.createElement('h3');
-    let roundResults = `You chose ${playerChoice} and computer choses ${compChoice}. Round result: `;
+    let roundResults = `Computer choses ${compChoice}. Round result: `;
     
     //check if the two are equal 
     if (compChoice === playerChoice){
